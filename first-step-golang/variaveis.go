bc81925c97c4d817edb8874794640e61d23852a0
Ana Alice Honório <79488448+Ana-Alice-Honorio@users.noreply.github.com>
2022-10-16 00:10:08 -0300

    Create variaveis.go

---
+++ first-step-golang/variaveis.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    // Strings, que podem ser adicionadas juntas com `+`.
+    fmt.Println("go" + "lang")
+
+    // Inteiros e floats.
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+    
+    // Booleanos, com operadores booleanos como você esperaria.
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
+
