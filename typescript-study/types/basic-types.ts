72534e52230269d5567403fa73c2d397f1622fc1
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2022-11-02 15:11:29 -0300

    add editions

--- typescript-study/types/basic-types.ts
+++ typescript-study/types/basic-types.ts
@@ -1,14 +1,14 @@
-var isWinter : boolean = false;
+var isWinter: boolean = false;
 // isWinter = 123;
 
-var count : 5 = true;
-var name : String = "Bran";
+var count: 5 = true;
+var name: String = "Bran";
 
-var names : any[] = ["Jon", "Rickon", "Arya"];
+var names: any[] = ["Jon", "Rickon", "Arya"];
 
 enum Starks {Jon, Bran, Eddard, Catlyn};
 
-var cat : Starks = Starks.Catlyn;
+var cat: Starks = Starks.Catlyn;
 
 function getName() : void{
     console.log("Winter is coming!");
--- typescript-study/types/classes.ts
+++ typescript-study/types/classes.ts
@@ -1,5 +1,13 @@
 class Stark {
     name: string;
+    static castle: string = "Winterfel";
+    saying: string;
+
+    constructor(){
+        this.saying = Stark.castle;
+    }
 }
 
 var ned = new Stark();
+ned.saying = "Winter is coming";
+console.log(Stark.castle);
